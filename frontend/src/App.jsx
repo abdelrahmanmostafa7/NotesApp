21babf6fbe79ae6fedf5970f2b759be201c35ad5
boyka <abdelrahmanzaki747@gmail.com>
2024-07-20 02:01:04 +0300

    setup&style login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUP/SignUp";
-
+import "./index.css";
 
 const routes = (
   <Router>
     <Routes>
       <Route path="/" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
-      <Route path="/signin" exact element={<SignUp />} />
+      <Route path="/signup" exact element={<SignUp />} />
     </Routes>
   </Router>
 );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Color used 
+      colors:{
+        primary:"#2B85FF",
+        secondary:"EF863E"
+      }
+    },
   },
   plugins: [],
 };
-
